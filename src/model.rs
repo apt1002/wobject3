be7ddb648089d38dc63c14eef4c7c4f03e59fe94
Alistair Turnbull <apt1002@sheep>
2025-12-23 16:00:51 +0000

    Shrink `model` back down to one source file.

--- src/model.rs
+++ src/model.rs
@@ -2,13 +2,28 @@ use std::{fmt};
 use std::rc::{Rc};
 use std::num::{Wrapping};
 
-use super::{Map};
+/// Represents a Welly constructor.
+///
+/// In source code, a `Tag`s is written as a name consisting only of capital
+/// letters, digits and underscores and not beginning with a digit.
+// TODO: Represent as a 64-bit integer.
+pub type Tag = Rc<str>;
 
-// ----------------------------------------------------------------------------
+/// Represents a Welly name.
+///
+/// In source code, a `Name`s is written as letters, digits and underscores,
+/// not beginning with a digit, and not a [`Tag`].
+// TODO: Represent as a 64-bit integer.
+pub type Name = Rc<str>;
+
+/// Represents a map from `Name` to `T`.
+pub type Map<T> = std::collections::HashMap<Name, T>;
 
 /// A 64-bit integer type with wrapping arithmetic.
 #[allow(non_camel_case_types)]
 pub type w64 = Wrapping<u64>;
+
+// ----------------------------------------------------------------------------
 
 /// A 64-bit word that is not a pointer.
 #[derive(Copy, Clone)]
